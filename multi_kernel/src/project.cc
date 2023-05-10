c3262a2c7033983beba73de94d5412ae11126427
Puya Amiri <pooya.w@gmail.com>
2023-05-10 18:12:58 +0200

    fix indention

--- multi_kernel/src/project.cc
+++ multi_kernel/src/project.cc
@@ -42,49 +42,50 @@ simpleGraph mygraph;
 
 int main(void) {
 
-	std::ifstream input_file(IN_FILE_ADR);
-	std::string in_line;
-	long int sample_size;
-	if (input_file.is_open()) { 
-		for (sample_size = 0; std::getline(input_file, in_line); sample_size++);
-	} else {
-		std::cerr << "Can't open input file!";
-	}
-	input_file.close();
+    std::ifstream input_file(IN_FILE_ADR);
+    std::string in_line;
+    long int sample_size;
+    if (input_file.is_open()) {
+        for (sample_size = 0; std::getline(input_file, in_line); sample_size++);
+    } else {
+        std::cerr << "Can't open input file!";
+    }
+    input_file.close();
 
-	long int iter_num = sample_size/4;
+    long int iter_num = sample_size/4;
 
-	mygraph.init();
-	std::cout << "Graph initialized" <<std::endl;
-	mygraph.run(iter_num);
-	std::cout << "Graph executed " << iter_num << " times"<< std::endl;
-	mygraph.end();
-	std::cout << "Graph end." <<std::endl;
-	std::string out_line;
-	std::string gold_line;
-	std::ifstream output_file(X86_OUT_FILE_ADR);
-	std::ifstream gold_file(GOLD_FILE_ADR);
-	if(output_file.is_open()) {
-		long int sample_num;
-		while(std::getline(output_file, out_line)) {
-			sample_num++;
-			if (gold_file.is_open()){
-				std::getline(gold_file, gold_line);
-				out_line.erase(std::remove(out_line.begin(), out_line.end(), ' '), out_line.end());
-				gold_line.erase(std::remove(gold_line.begin(), gold_line.end(), ' '), gold_line.end());
-				if(out_line != gold_line) {
-					std::cout <<"sample #"<< sample_num << " : " << out_line << "!=" << gold_line << std::endl;
-				}
-			} else {
-				std::cerr << "cannot open output.txt!";
-			}
+    mygraph.init();
+    std::cout << "Graph initialized" <<std::endl;
+    mygraph.run(iter_num);
+    std::cout << "Graph executed " << iter_num << " times"<< std::endl;
+    mygraph.end();
+    std::cout << "Graph end." <<std::endl;
 
-		}
+    std::string out_line;
+    std::string gold_line;
+    std::ifstream output_file(X86_OUT_FILE_ADR);
+    std::ifstream gold_file(GOLD_FILE_ADR);
 
-		output_file.close();
-		gold_file.close();
-	} else {
-		std::cerr << "cannot open output.txt!";
-	}
-	return 0;
+    if(output_file.is_open()) {
+        long int sample_num;
+        while(std::getline(output_file, out_line)) {
+            sample_num++;
+            if (gold_file.is_open()){
+                std::getline(gold_file, gold_line);
+                out_line.erase(std::remove(out_line.begin(), out_line.end(), ' '), out_line.end());
+                gold_line.erase(std::remove(gold_line.begin(), gold_line.end(), ' '), gold_line.end());
+                if(out_line != gold_line) {
+                    std::cout <<"sample #"<< sample_num << " : " << out_line << "!=" << gold_line << std::endl;
+                }
+            } else {
+                std::cerr << "cannot open output.txt!";
+            }
+        }
+
+        output_file.close();
+        gold_file.close();
+    } else {
+        std::cerr << "cannot open output.txt!";
+    }
+    return 0;
 }
