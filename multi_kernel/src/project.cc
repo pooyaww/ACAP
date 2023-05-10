ee7edfb334286bb043c9674a118912f3a9208b12
Puya <pooya.w@gmail.com>
2023-05-10 18:09:02 +0200

    a compllete test bench for x86 simulation

--- multi_kernel/src/project.cc
+++ multi_kernel/src/project.cc
@@ -1,5 +1,13 @@
 #include <adf.h>
-#define iter_num 4
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#define GOLD_FILE_ADR "./data/golden.txt"
+#define IN_FILE_ADR "data/input.txt"
+#define OUT_FILE_ADR "data/output.txt"
+#define X86_OUT_FILE_ADR "./x86simulator_output/data/output.txt"
+
 void aie_read(input_stream_int32* in, output_stream_int32* out);
 void aie_write(input_stream_int32* in, output_stream_int32* out);
 
@@ -12,8 +20,8 @@ public:
     adf::output_plio out;
 
     simpleGraph() {
-        in = adf::input_plio::create(adf::plio_32_bits, "data/input.txt");
-        out = adf::output_plio::create(adf::plio_32_bits, "data/output.txt");
+        in = adf::input_plio::create(adf::plio_32_bits, IN_FILE_ADR);
+        out = adf::output_plio::create(adf::plio_32_bits, OUT_FILE_ADR);
 
         read = adf::kernel::create(aie_read);
         write = adf::kernel::create(aie_write);
@@ -33,13 +41,50 @@ public:
 simpleGraph mygraph;
 
 int main(void) {
-	//TODO: increase the run size for all samples dynamically
-	//TODO: use popen to diff between generated files
-  mygraph.init();
-  std::cout << "Graph initialized" <<std::endl;
-  mygraph.run(iter_num);
-  std::cout << "Graph executed " << iter_num << " times"<< std::endl;
-  mygraph.end();
-  std::cout << "Graph end." <<std::endl;
-  return 0;
+
+	std::ifstream input_file(IN_FILE_ADR);
+	std::string in_line;
+	long int sample_size;
+	if (input_file.is_open()) { 
+		for (sample_size = 0; std::getline(input_file, in_line); sample_size++);
+	} else {
+		std::cerr << "Can't open input file!";
+	}
+	input_file.close();
+
+	long int iter_num = sample_size/4;
+
+	mygraph.init();
+	std::cout << "Graph initialized" <<std::endl;
+	mygraph.run(iter_num);
+	std::cout << "Graph executed " << iter_num << " times"<< std::endl;
+	mygraph.end();
+	std::cout << "Graph end." <<std::endl;
+	std::string out_line;
+	std::string gold_line;
+	std::ifstream output_file(X86_OUT_FILE_ADR);
+	std::ifstream gold_file(GOLD_FILE_ADR);
+	if(output_file.is_open()) {
+		long int sample_num;
+		while(std::getline(output_file, out_line)) {
+			sample_num++;
+			if (gold_file.is_open()){
+				std::getline(gold_file, gold_line);
+				out_line.erase(std::remove(out_line.begin(), out_line.end(), ' '), out_line.end());
+				gold_line.erase(std::remove(gold_line.begin(), gold_line.end(), ' '), gold_line.end());
+				if(out_line != gold_line) {
+					std::cout <<"sample #"<< sample_num << " : " << out_line << "!=" << gold_line << std::endl;
+				}
+			} else {
+				std::cerr << "cannot open output.txt!";
+			}
+
+		}
+
+		output_file.close();
+		gold_file.close();
+	} else {
+		std::cerr << "cannot open output.txt!";
+	}
+	return 0;
 }
