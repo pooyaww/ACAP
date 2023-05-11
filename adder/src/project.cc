4816daf8513ee7a859940154f3debcc3134d88b9
Puya <pooya.w@gmail.com>
2023-05-11 12:37:51 +0200

    adder added

--- adder/src/project.cc
+++ adder/src/project.cc
@@ -3,48 +3,50 @@
 #include <string>
 #include <fstream>
 
+
 #define LANE_SIZE 4
 #define GOLD_FILE_ADR "./data/golden.txt"
-#define IN_FILE_ADR "data/input.txt"
+#define IN0_FILE_ADR "data/input0.txt"
+#define IN1_FILE_ADR "data/input1.txt"
 #define OUT_FILE_ADR "data/output.txt"
 #define X86_OUT_FILE_ADR "./x86simulator_output/data/output.txt"
 
-void aie_read(input_stream_int32* in, output_stream_int32* out);
-void aie_write(input_stream_int32* in, output_stream_int32* out);
+using namespace adf;
 
-class simpleGraph : public adf::graph {
-private:
-    adf::kernel read;
-    adf::kernel write;
-public:
-    adf::input_plio in;
-    adf::output_plio out;
+void aie_adder(input_stream_int32* in0, input_stream_int32* in1, output_stream_int32* out);
+
+class simpleGraph : public graph {
+   private:
+    kernel adder;
+
+   public:
+    input_plio pl_in0, pl_in1;
+    output_plio pl_out;
 
     simpleGraph() {
-        in = adf::input_plio::create(adf::plio_32_bits, IN_FILE_ADR);
-        out = adf::output_plio::create(adf::plio_32_bits, OUT_FILE_ADR);
+        adder = kernel::create(aie_adder);
 
-        read = adf::kernel::create(aie_read);
-        write = adf::kernel::create(aie_write);
+        pl_in0 = input_plio::create("DataIn0", adf::plio_32_bits, IN0_FILE_ADR);
+        pl_in1 = input_plio::create("DataIn1", adf::plio_32_bits, IN1_FILE_ADR);
+        pl_out = output_plio::create("DataOut", adf::plio_32_bits, OUT_FILE_ADR);
 
-	adf::connect<adf::stream> net0(in.out[0], read.in[0]);
-	adf::connect<adf::stream> net1(read.out[0], write.in[0]);
-	adf::connect<adf::stream> net2(write.out[0], out.in[0]);
+        connect<stream> net0(pl_in0.out[0], adder.in[0]);
+        connect<stream> net1(pl_in1.out[0], adder.in[1]);
+        connect<stream> net2(adder.out[0], pl_out.in[0]);
 
-	adf::source(read) = "kernels.cc";
-	adf::source(write) = "kernels.cc";
+        source(adder) = "kernel.cc";
 
-	adf::runtime<ratio>(read)  = 1;
-	adf::runtime<ratio>(write) = 1;
-    }
+        runtime<ratio>(adder) = 0.1;
+    };
 };
 
 simpleGraph mygraph;
 
-int main(void) {
+int main(int argc, char** argv) {
+
     adf::return_code ret;
 
-    std::ifstream input_file(IN_FILE_ADR);
+    std::ifstream input_file(IN1_FILE_ADR);
     std::string in_line;
     long int sample_size;
     if (input_file.is_open()) {
@@ -79,16 +81,16 @@ int main(void) {
     if(output_file.is_open()) {
         long int sample_num;
         while(std::getline(output_file, out_line)) {
-            sample_num++;
+	    out_line.erase(std::remove(out_line.begin(), out_line.end(), ' '), out_line.end());
             if (gold_file.is_open()){
                 std::getline(gold_file, gold_line);
-                out_line.erase(std::remove(out_line.begin(), out_line.end(), ' '), out_line.end());
-                gold_line.erase(std::remove(gold_line.begin(), gold_line.end(), ' '), gold_line.end());
-                if(out_line != gold_line) {
-                    std::cout <<"sample #"<< sample_num << " : " << out_line << "!=" << gold_line << std::endl;
-                }
             } else {
-                std::cerr << "cannot open output.txt!";
+                std::cerr << "cannot open gold.txt!";
+            }
+            sample_num++;
+            gold_line.erase(std::remove(gold_line.begin(), gold_line.end(), ' '), gold_line.end());
+            if(out_line != gold_line) {
+                std::cout <<"sample #"<< sample_num << " : " << out_line << "!=" << gold_line << std::endl;
             }
         }
 
