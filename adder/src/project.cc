8d579c684710a5f47ac95e29e655cd3a0a4ef6aa
Puya <pooya.w@gmail.com>
2023-06-22 16:53:20 +0200

    fix simulation macros

--- adder/src/project.cc
+++ adder/src/project.cc
@@ -9,7 +9,14 @@
 #define IN0_FILE_ADR "data/input0.txt"
 #define IN1_FILE_ADR "data/input1.txt"
 #define OUT_FILE_ADR "data/output.txt"
-#define X86_OUT_FILE_ADR "./x86simulator_output/data/output.txt"
+
+#if defined(__AIESIM__)
+  #define SIM_OUT_FILE_ADR "./aiesimulator_output/data/output.txt"
+#endif
+
+#if defined(__X86SIM__)
+    #define SIM_OUT_FILE_ADR "./x86simulator_output/data/output.txt"
+#endif
 
 using namespace adf;
 
@@ -75,7 +82,8 @@ int main(int argc, char** argv) {
 
     std::string out_line;
     std::string gold_line;
-    std::ifstream output_file(X86_OUT_FILE_ADR);
+    //std::ifstream output_file(X86_OUT_FILE_ADR);
+    std::ifstream output_file(SIM_OUT_FILE_ADR);
     std::ifstream gold_file(GOLD_FILE_ADR);
 
     if(output_file.is_open()) {
