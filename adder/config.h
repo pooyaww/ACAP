c689e48958017fe946a29717e8d102961a6aa01e
Puya <pooya.w@gmail.com>
2023-06-24 09:42:04 +0200

    simplify

--- adder/config.h
+++ adder/config.h
@@ -1,6 +1,5 @@
 
 #define LANE_SIZE 4
-#define WINDOW_SIZE 2048 //sample size, one iteration of graph is enough
-using TYPE = int32_t;
-
+#define TYPE int32_t
 #define WINDOW
+#define WINDOW_SIZE 2048 //sample size, one iteration of graph is enough
