6f17aa2e2d4b988b2468adbdb94d6ebdb163a917
Puya <pooya.w@gmail.com>
2024-05-09 13:10:55 +0200

    mosaic test

--- mosaique_test/Work/pthread/PthreadSimXmc.cpp.d
+++
@@ -1,4 +0,0 @@
-PthreadSimXmc.cpp.o: PthreadSimXmc.cpp \
- /opt/xilinx/Vitis/2022.2/aietools/include/adf/x86sim/symbolVisibility.h
-
-/opt/xilinx/Vitis/2022.2/aietools/include/adf/x86sim/symbolVisibility.h:
--- mosaique_test/Work/pthread/wrap_kernel.cxx
+++
@@ -1,8 +0,0 @@
-#include "adf.h"
-#include "../../src/kernel.cxx"
-void b1_kernel_wrapper(x86sim::stream_internal * arg0, x86sim::stream_internal * arg1)
-{
-  auto _arg0 = (input_stream_int32 *)(arg0);
-  auto _arg1 = (output_stream_int32 *)(arg1);
-  return _3309(_arg0, _arg1);
-}
